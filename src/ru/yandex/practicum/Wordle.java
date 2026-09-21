e83f758a72512b90294f8207552f5aafbae4c4fb
Anastasia Chernyshova <nastya-chernyshova03@mail.ru>
2026-09-22 00:16:42 +0300

    Исправление 3. После тестов

--- src/ru/yandex/practicum/Wordle.java
+++ src/ru/yandex/practicum/Wordle.java
@@ -76,7 +76,7 @@ public class Wordle {
                 }
             }
         } catch (IOException e) {
-            System.out.println(e.getMessage());
+            System.err.println(e.getMessage());
         }
     }
 }
